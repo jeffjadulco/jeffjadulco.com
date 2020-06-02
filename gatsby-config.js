3f4028744e1a95782632e73bc8c7107754b2503c
Jeff Jadulco <jeffjadulco.work@gmail.com>
2020-06-02 08:15:05 +0800

    Update template contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jeff Jadulco`,
+    description: `Jeff Jadulco is a game and javascript developer`,
+    author: `Jeff Jadulco`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
