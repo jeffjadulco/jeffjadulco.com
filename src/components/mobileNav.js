a0dee96e70dec9ce22e90f0493f5e4297bae9035
Jeff Jadulco <jeffjadulco.work@gmail.com>
2020-06-06 17:50:49 +0800

    Fix mobile nav scrolling on long pages

--- src/components/mobileNav.js
+++ src/components/mobileNav.js
@@ -5,6 +5,20 @@ import ThemeToggle from "./themeToggle"
 const MobileNav = () => {
   const [navShow, setNavShow] = React.useState(false)
 
+  React.useEffect(
+    _ => {
+      const body = document.body
+      if (typeof body !== "undefined") {
+        if (navShow) {
+          body.style.setProperty("overflow", "hidden")
+        } else {
+          body.style.removeProperty("overflow")
+        }
+      }
+    },
+    [navShow]
+  )
+
   return (
     <div className="md:hidden text-xl">
       <button
@@ -17,32 +31,32 @@ const MobileNav = () => {
         {navShow ? (
           <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         ) : (
           <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         )}
       </button>
       {navShow && (
         <div>
-          <div className="w-full h-screen absolute bg-primary opacity-75"></div>
+          <div className="w-full h-screen fixed bg-primary opacity-75"></div>
           <button
             type="button"
-            className="w-full h-screen absolute cursor-auto"
+            className="w-full h-screen fixed cursor-auto"
             onClick={e => {
               setNavShow(!navShow)
             }}
           ></button>
-          <nav className="right-0 h-screen mt-auto py-48 px-5 absolute flex flex-col items-end text-primary tracking-widest">
+          <nav className="right-0 h-screen mt-auto py-48 px-5 fixed flex flex-col items-end text-primary tracking-widest">
             <div className="flex-grow">
               <Link to="/">HOME</Link>
             </div>
