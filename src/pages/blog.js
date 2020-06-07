72880faa0f7a9bad08501e2d104c4d50e46f08a0
Jeff Jadulco <jeffjadulco.work@gmail.com>
2020-06-07 17:01:43 +0800

    Add temp blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Blog = () => {
+  return (
+    <Layout>
+      <h1 className="mt-12 mb-2 text-2xl font-serif font-medium text-primary text-center">
+        Blog is coming soon!
+      </h1>
+    </Layout>
+  )
+}
+
+export default Blog
