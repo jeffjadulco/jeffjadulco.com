084edd802284dc64dfc2b03cb4af0e4ec70cec2c
Jeff Jadulco <jeffjadulco.work@gmail.com>
2020-06-12 15:14:35 +0800

    Support external trigger for ThemeContext

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -20,46 +20,52 @@ const getInitialTheme = _ => {
 
 export const ThemeContext = React.createContext()
 
-export const ThemeProvider = ({ children }) => {
+export const ThemeProvider = ({ initialTheme, children }) => {
   const [theme, setTheme] = React.useState(getInitialTheme)
+
+  const rawSetTheme = theme => {
+    const root = window.document.documentElement
+    const isDark = theme === "dark"
+
+    // Apply theme to css variables
+    root.style.setProperty(
+      "--color-bg-primary",
+      isDark ? COLORS.dark.backgroundPrimary : COLORS.light.backgroundPrimary
+    )
+
+    root.style.setProperty(
+      "--color-bg-secondary",
+      isDark
+        ? COLORS.dark.backgroundSecondary
+        : COLORS.light.backgroundSecondary
+    )
+
+    root.style.setProperty(
+      "--color-bg-tertiary",
+      isDark ? COLORS.dark.backgroundTertiary : COLORS.light.backgroundTertiary
+    )
+
+    root.style.setProperty(
+      "--color-text-primary",
+      isDark ? COLORS.dark.textPrimary : COLORS.light.textPrimary
+    )
+
+    root.style.setProperty(
+      "--color-text-secondary",
+      isDark ? COLORS.dark.textSecondary : COLORS.light.textSecondary
+    )
+
+    // Save theme to localStorage
+    localStorage.setItem("color-theme", isDark ? "dark" : "light")
+  }
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme)
+  }
 
   React.useEffect(
     _ => {
-      const root = window.document.documentElement
-      const isDark = theme === "dark"
-
-      // Apply theme to css variables
-      root.style.setProperty(
-        "--color-bg-primary",
-        isDark ? COLORS.dark.backgroundPrimary : COLORS.light.backgroundPrimary
-      )
-
-      root.style.setProperty(
-        "--color-bg-secondary",
-        isDark
-          ? COLORS.dark.backgroundSecondary
-          : COLORS.light.backgroundSecondary
-      )
-
-      root.style.setProperty(
-        "--color-bg-tertiary",
-        isDark
-          ? COLORS.dark.backgroundTertiary
-          : COLORS.light.backgroundTertiary
-      )
-
-      root.style.setProperty(
-        "--color-text-primary",
-        isDark ? COLORS.dark.textPrimary : COLORS.light.textPrimary
-      )
-
-      root.style.setProperty(
-        "--color-text-secondary",
-        isDark ? COLORS.dark.textSecondary : COLORS.light.textSecondary
-      )
-
-      // Save theme to localStorage
-      localStorage.setItem("color-theme", isDark ? "dark" : "light")
+      rawSetTheme(theme)
     },
     [theme]
   )
