f7433e73961ffed088155ae78ba3d931723a5bfc
Jeff Jadulco <jeffjadulco.work@gmail.com>
2020-06-12 13:50:09 +0800

    Configure Storybook config for Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
