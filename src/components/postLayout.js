e38de66b34f81282f8746f8cc7ee2d73f304c4d4
Jeff Jadulco <dev@jeffjadulco.com>
2020-07-18 11:40:14 +0800

    Disable smooth scroll. Improve mobile readability

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -19,7 +19,7 @@ const PostLayout = ({ data: { mdx } }) => {
     <Layout>
       <SEO blog title={mdx.frontmatter.title} description={mdx.excerpt} />
       <div className="flex justify-between mt-12 mb-12 relative">
-        <article className="prose prose-sm sm:prose md:prose-lg min-w-0 max-w-none">
+        <article className="prose sm:prose md:prose-lg min-w-0 max-w-none">
           <div className="">
             <BlogTitleInfo
               date={mdx.frontmatter.date}
