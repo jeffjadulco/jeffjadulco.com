4fc18702092d6fbdbd3866a08123cc10ead54d67
Jeff Jadulco <dev@jeffjadulco.com>
2020-08-16 22:15:42 +0800

    :sparkles: Add skills section under Projects page

---
+++ src/data/skills.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    label: "Game Development Skills",
+    description:
+      "Skills I acquired during 5+ years of being in game development industry",
+    categories: [
+      {
+        title: "Unreal Engine 4 Game Development",
+        skills: [
+          "C++ and Blueprint Programming",
+          "UMG and Complex UI Systems",
+          "Multiplayer and Network Programming",
+          "3rd Party Library and API Integration",
+          "Online Services Integration",
+        ],
+      },
+      {
+        title: "Unity Game Development",
+        skills: [
+          "C# Programming",
+          "Complex System Design and Architecture",
+          "Complex UI Systems",
+          "Editor Tooling",
+          "3rd Party Library and API Integration",
+        ],
+      },
+    ],
+  },
+  {
+    label: "Web Development Skills",
+    description: "Skills I learned during the quarantine",
+    categories: [
+      {
+        title: "Front-end Development",
+        skills: [
+          "React: CRA, Gatsby, and Next",
+          "Tailwind CSS and Component Libraries",
+          "Jest & React Testing Library",
+          "Design Systems w/Storybook",
+          "SEO-friendly and Accessible Designs",
+        ],
+      },
+      {
+        title: "Backend Development",
+        skills: ["Express", "Socket.io", "Firebase and Firestore"],
+      },
+    ],
+  },
+]
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,16 +4,30 @@ import SEO from "../components/seo"
 import { Heading } from "../components/atoms"
 import Contact from "../components/contact"
 import ProjectGrid from "../components/projectGrid"
+import SkillGrid from "../components/skillGrid"
+import skills from "../data/skills"
 
 const Projects = () => {
   return (
     <Layout activePage="projects">
       <SEO title="Projects" />
       <Heading>Projects</Heading>
-      <p className="mb-12 text-secondary">
+      <p className="-mt-3 mb-8 text-tertiary">
         Collection of web and game development side projects.
       </p>
       <ProjectGrid />
+      <div className="mt-12 flex flex-col space-y-0">
+        {skills.map(data => {
+          return (
+            <SkillGrid
+              key={data.label}
+              heading={data.label}
+              description={data.description}
+              categories={data.categories}
+            />
+          )
+        })}
+      </div>
       <Contact />
     </Layout>
   )
