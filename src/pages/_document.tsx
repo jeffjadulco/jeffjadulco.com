07e1011f78f454d9093425e7f730ae724df7b450
Jeff Jadulco <dev@jeffjadulco.com>
2021-05-23 11:30:10 +0800

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" type="icon" />
           <link
             rel="preload"
             href="/fonts/Inter.var.woff2"
